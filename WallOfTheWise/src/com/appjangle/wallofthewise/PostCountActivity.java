6ceb4beef80bc18e8f2314489861925e2ffe1dd7
denley <denleybihari@gmail.com>
2013-06-12 15:21:12 +0930

    Added node monitor for automatically updating the data when changed. Properly threaded network connections.

--- WallOfTheWise/src/com/appjangle/wallofthewise/PostCountActivity.java
+++ WallOfTheWise/src/com/appjangle/wallofthewise/PostCountActivity.java
@@ -3,10 +3,14 @@ package com.appjangle.wallofthewise;
 import io.nextweb.Link;
 import io.nextweb.Node;
 import io.nextweb.Session;
+import io.nextweb.common.Interval;
+import io.nextweb.common.Monitor;
+import io.nextweb.common.MonitorContext;
 import io.nextweb.fn.Closure;
 import io.nextweb.fn.ExceptionListener;
 import io.nextweb.fn.ExceptionResult;
 import io.nextweb.jre.Nextweb;
+import io.nextweb.operations.callbacks.NodeListener;
 
 import java.util.Map;
 
@@ -23,9 +27,8 @@ import com.appjangle.demos.appjanglejavademo.Calculations;
 
 public class PostCountActivity extends Activity {
 
+	// Required argument for displaying this Activity
 	public static final String ARG_URL = "url";
-	
-	private static final String TAG = "NextWeb";
 	
 	
 	@Override
@@ -41,71 +44,112 @@ public class PostCountActivity extends Activity {
 		
 		setContentView(R.layout.activity_post_count_loading);
 		
-		// Load the content from the url. Thread network connections
-		new Thread(){
-			public void run(){
-				loadContent(intent.getStringExtra(ARG_URL));
-			}
-		}.start();
+		// Load the content from the url
+		loadContent(intent.getStringExtra(ARG_URL));
 	}
-	
 	
 	/** Loads the post data from the given url and displays it in the list */
 	private void loadContent(final String url){
 		final Session session = Nextweb.createSession();
         try {
            
-            Link posts = session.node(url);
+            final Link posts = session.node(url);
             
             // Catch errors
             posts.catchExceptions(new ExceptionListener() {
                 public void onFailure(ExceptionResult r) {
-                	// TODO: Show error to user
-                    Log.e(TAG, "Error", r.exception());
+                	showError(r.exception());
                 }
             });
 
+            // Data callback
             posts.get(new Closure<Node>() {
                 public void apply(Node n) {
-                	final Calculations c = new Calculations(n.getSession());
-                    final Map<String, Integer> results = c.calculatePostsPerUser(n);
-                    
-                    // Show the data to the user
-                    runOnUiThread(new Runnable(){
-                    	public void run(){
-                    		showContent(results);
-                    	}
-                    });
+                	loadContent(n);
+                	installMonitor(n);
                 }
             });
         } catch (Throwable t) {
-        	// TODO: Show error to user
-            Log.e(TAG, "Error", t);
-            finish();
+        	showError(t);
         }
 	}
 	
+	/** Shows an error to the user. Can be called from any thread. */
+	private void showError(final Throwable t){
+		runOnUiThread(new Runnable(){
+			public void run(){
+				Log.e("NextWeb", "Error", t);
+				final TextView error = new TextView(PostCountActivity.this);
+				Throwable cause = t;
+				while(cause.getCause()!=null){
+					cause = cause.getCause();
+				}
+				
+				error.setText("Error loading data:\n"+cause.getMessage());
+		    	setContentView(error);
+			}
+		});
+	}
 	
-	/** Shows the given name & posts data in a table */
-	private void showContent(final Map<String, Integer> content){
-		setContentView(R.layout.activity_post_count);
-		final TableLayout table = (TableLayout) findViewById(R.id.tableRoot);
-		final LayoutInflater inflater = getLayoutInflater();
-		
-		// add a row for each piece of data
-		for(final String key:content.keySet()){
-			final Integer count = content.get(key);
-			
-			// Create new row
-			final TableRow row = (TableRow) inflater.inflate(R.layout.table_row_post_count, null);
-			table.addView(row);
-			final TextView name = (TextView) row.findViewById(R.id.name);
-			final TextView posts = (TextView) row.findViewById(R.id.posts);
-			
-			// Set data
-			name.setText(key);
-			posts.setText(count.toString());
-		}
+	/** Sets up a monitor to update the view when new posts are added */
+	private void installMonitor(final Node posts) {
+	    Session monitorSession = Nextweb.createSession();
+	    monitorSession.node(posts).monitor().setInterval(Interval.FAST)
+	                                        .setDepth(2)
+	                                        .addListener(new NodeListener() {
+
+	        public void onWhenNodeChanged(MonitorContext context) {
+	            posts.reload(2).get(new Closure<Node>() {
+	                @Override
+	                public void apply(Node n) {
+	                     loadContent(posts);
+	                }
+	            });
+	        }
+	    }).get(new Closure<Monitor>() {
+	        public void apply(Monitor m) {}
+	    });
+	}
+	
+	/** Loads and then displays the post count for the data contained in the given node */
+	private void loadContent(final Node n){
+		// Thread network connections
+    	new Thread(){
+    		public void run(){
+    			final Calculations c = new Calculations(n.getSession());
+                final Map<String, Integer> results = c.calculatePostsPerUser(n);
+                
+                // Show the data to the user
+                loadContent(results);
+    		}
+    	}.start();
+	}
+	
+	
+	/** Shows the given name & posts data in a table. Can be called from any thread */
+	private void loadContent(final Map<String, Integer> content){
+		runOnUiThread(new Runnable(){
+        	public void run(){
+				setContentView(R.layout.activity_post_count);
+				final TableLayout table = (TableLayout) findViewById(R.id.tableRoot);
+				final LayoutInflater inflater = getLayoutInflater();
+				
+				// add a row for each piece of data
+				for(final String key:content.keySet()){
+					final Integer count = content.get(key);
+					
+					// Create new row
+					final TableRow row = (TableRow) inflater.inflate(R.layout.table_row_post_count, null);
+					table.addView(row);
+					final TextView name = (TextView) row.findViewById(R.id.name);
+					final TextView posts = (TextView) row.findViewById(R.id.posts);
+					
+					// Set data
+					name.setText(key);
+					posts.setText(count.toString());
+				}
+        	}
+        });
 	}
 	
 }
